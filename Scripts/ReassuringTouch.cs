f007f70e5be931423d9ffa3829168c72d22874f4
SukiMiyako <1272473049@qq.com>
2026-04-10 21:00:44 +0800

    优化卡牌描述的观感，添加新卡牌“这份触摸，令人安心”和新能力“安心之触”

---
+++ Scripts/ReassuringTouch.cs
@@ -0,0 +1,45 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using Tsukiyuki_Miyako.MiyakoModCode.Character;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+using Tsukiyuki_Miyako.MiyakoModCode.Powers;
+
+namespace TsukiyukiMiyako.Scripts.Cards;
+
+[Pool(typeof(MiyakoCardPool))]
+public sealed class ReassuringTouch : CustomCardModel
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Uncommon;
+    private const TargetType targetType = TargetType.Self;
+    private const bool shouldShowInCardLibrary = true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(8m, ValueProp.Move)
+    ];
+
+    public override string PortraitPath => $"res://Tsukiyuki Miyako/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+    public override bool GainsBlock => true;
+
+    public ReassuringTouch() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        decimal blockAmount = await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<SenseiPower>(Owner.Creature, 1, Owner.Creature, this);
+        var power = await PowerCmd.Apply<ReassuringTouchPower>(Owner.Creature, 1, Owner.Creature, this);
+        power?.SetBlock(blockAmount);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(4);
+    }
+}
---
+++ Scripts/ReassuringTouchPower.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TsukiyukiMiyako.Scripts;
+
+public sealed class ReassuringTouchPower : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => new List<IHoverTip>() { HoverTipFactory.Static(StaticHoverTip.Block) };
+    public override bool IsInstanced => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => new List<DynamicVar>() { new BlockVar(0m, ValueProp.Unpowered) };
+
+    public void SetBlock(decimal block)
+    {
+        AssertMutable();
+        base.DynamicVars.Block.BaseValue = block;
+    }
+
+    // 核心修正：下回合开始触发（你要的效果）
+    public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
+    {
+        if (side == base.Owner.Side)
+        {
+            Flash();
+            await CreatureCmd.GainBlock(base.Owner, base.DynamicVars.Block, null);
+            await PowerCmd.Decrement(this);
+        }
+    }
+}
