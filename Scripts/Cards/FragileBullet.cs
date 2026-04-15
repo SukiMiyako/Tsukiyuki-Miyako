b6e13f0b0d56e5b8c78dcabe308175fa4f127460
SukiMiyako <1272473049@qq.com>
2026-04-15 11:44:08 +0800

    新增卡片“易碎弹”，补充了之前添加的卡牌的卡图

---
+++ Scripts/Cards/FragileBullet.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using Tsukiyuki_Miyako.MiyakoModCode.Character;
+
+namespace TsukiyukiMiyako.Scripts.Cards;
+
+[Pool(typeof(MiyakoCardPool))]
+public sealed class FragileBullet : CustomCardModel
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => new List<IHoverTip>
+    {
+        HoverTipFactory.FromPower<VulnerablePower>()
+    };
+    public override IEnumerable<CardKeyword> CanonicalKeywords => new[]
+    {
+        CardKeyword.Exhaust,
+        MyKeywords.Equipment
+    };
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => new List<DynamicVar>
+    {
+        new DamageVar(4m, ValueProp.Move),
+        new PowerVar<VulnerablePower>(2m)
+    };
+
+    public override string PortraitPath => $"res://Tsukiyuki Miyako/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+
+    public FragileBullet()
+        : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target!)
+            .Execute(choiceContext);
+
+        await PowerCmd.Apply<VulnerablePower>(cardPlay.Target!, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2m);
+        DynamicVars["VulnerablePower"].UpgradeValueBy(1m);
+    }
+}
