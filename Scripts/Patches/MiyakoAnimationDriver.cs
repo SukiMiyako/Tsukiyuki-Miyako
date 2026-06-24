ac354492b1fd67842287274e3e03d58e1350249e
SukiMiyako <1272473049@qq.com>
2026-06-24 13:45:20 +0800

    debug: 添加 TriggerDieOnScreen 调试日志

--- Scripts/Patches/MiyakoAnimationDriver.cs
+++ Scripts/Patches/MiyakoAnimationDriver.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Nodes.Combat;
 
 #nullable disable
@@ -56,19 +57,35 @@ internal static class MiyakoAnimationDriver
     /// </summary>
     public static void TriggerDieOnScreen(Node screen)
     {
+        Log.Debug("[Miyako] TriggerDieOnScreen called");
+
         Control creatureContainer = Traverse.Create(screen).Field<Control>("_creatureContainer").Value;
         if (creatureContainer == null)
+        {
+            Log.Debug("[Miyako] _creatureContainer is null");
             return;
+        }
 
+        Log.Debug($"[Miyako] Found container with {creatureContainer.GetChildCount()} children");
         foreach (Node child in creatureContainer.GetChildren(false))
         {
+            Log.Debug($"[Miyako] Child: {child.GetType().Name} '{child.Name}'");
             if (TryGetAnimatedSprite(child, out AnimatedSprite2D sprite) && sprite.SpriteFrames != null)
             {
                 string animName = FindFirstAnimation(sprite, "die", "hurt", "idle_loop", "idle");
                 if (animName != null)
                 {
+                    Log.Debug($"[Miyako] Playing anim '{animName}' on {child.Name}");
                     sprite.Play(animName, 1f, false);
                 }
+                else
+                {
+                    Log.Debug($"[Miyako] No die/hurt/idle animation found on {child.Name}");
+                }
+            }
+            else
+            {
+                Log.Debug($"[Miyako] No AnimatedSprite2D on {child.Name}");
             }
         }
     }
